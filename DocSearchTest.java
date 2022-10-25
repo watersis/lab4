cc3d2c3cb0c71b197d97536cffb167079f95084b
watersis <114371045+watersis@users.noreply.github.com>
2022-10-25 10:56:08 -0700

    Update DocSearchTest.java

--- DocSearchTest.java
+++ DocSearchTest.java
@@ -1,8 +1,23 @@
+
+
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.net.URI;
 import java.net.URISyntaxException;
-public class DocSearchServer{
-    @Test
-    
+import java.io.IOException;
+
+public class TestDocSearch {
+	@Test 
+	public void testIndex() throws URISyntaxException, IOException {
+    Handler h = new Handler("./technical/");
+    URI rootPath = new URI("http://localhost/");
+    assertEquals("There are 10 total files to search.", h.handleRequest(rootPath));
+	}
+	@Test 
+	public void testSearch() throws URISyntaxException, IOException {
+    Handler h = new Handler("./technical/");
+    URI rootPath = new URI("http://localhost/search?q=taxation");
+    String expect = "Found 3 paths:\n./technical/biomed/1471-2296-3-3.txt\n./technical/government/Gen_Account_Office/d01591sp.txt\n./technical/plos/journal.pbio.0020052.txt";
+    assertEquals(expect, h.handleRequest(rootPath));
+	}
 }
